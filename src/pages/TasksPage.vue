c3f01f59032135f293ac89a22deda81499f25699
Ketlina Puci <ketlina.puci@evolutivo.io>
2024-05-07 19:24:20 +0200

    add new component to insert data

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -4,13 +4,7 @@
             <div class="row">
                 <div class="col-md-8 offset-md-2">
                     <!-- Add new Task -->
-                    <div class="relative">
-                        <input
-                            type="text"
-                            class="form-control form-control-lg padding-right-lg"
-                            placeholder="+ Add new task. Press enter to save."
-                        />
-                    </div>
+                    <NewTask @added="handleAddedTask" />
                     <!-- List of uncompleted tasks -->
                     <Tasks :tasks="uncompletedTasks" />
 
@@ -32,8 +26,9 @@
 </template>
 <script setup>
 import { computed, onMounted, ref} from "vue";
-import { allTasks } from "../http/task-api";
+import { allTasks, createTask } from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue";
+import NewTask from "../components/tasks/NewTask.vue";
 const tasks = ref([])
 
 onMounted(async () => {
@@ -45,5 +40,9 @@ const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_comp
 const completedTasks = computed(() => tasks.value.filter(task => task.is_completed));
 const showToggleCompletedBtn = computed(() => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0);
 const completedTasksIsVisible = computed(() => uncompletedTasks.value.length == 0 || completedTasks.value.length > 0);
-const showCompletedTasks = ref(false)
+const showCompletedTasks = ref(false);
+const handleAddedTask = async (newTask) => {
+    const { data: createdTask } = await createTask(newTask);
+    tasks.value.unshift(createdTask.data);
+};
 </script>
