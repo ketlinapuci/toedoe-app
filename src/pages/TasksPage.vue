67eac1ea30d63bfe9b6fd0c6ae53e45a3ff4af2e
Ketlina Puci <ketlina.puci@evolutivo.io>
2024-05-15 19:05:59 +0200

    Implement pinia actions and getters in the components

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -4,14 +4,10 @@
             <div class="row">
                 <div class="col-md-8 offset-md-2">
                     <!-- Add new Task -->
-                    <NewTask @added="handleAddedTask" />
+                    <NewTask />
                     <!-- List of uncompleted tasks -->
-                    <Tasks
-                        :tasks="uncompletedTasks"
-                        @updated="handleUpdatedTask"
-                        @completed="handleCompletedTask"
-                        @removed="handleRemovedTask"
-                    />
+                    <Tasks :tasks="uncompletedTasks" />
+
                     <!-- show toggle button -->
                     <div class="text-center my-3" v-show="showToggleCompletedBtn">
                         <button class="btn btn-sm btn-secondary"
@@ -22,9 +18,6 @@
                     </div>
                     <!-- list of completed tasks -->
                     <Tasks :tasks="completedTasks" :show="completedTasksIsVisible && showCompletedTasks"
-                    @updated="handleUpdatedTask"
-                    @completed="handleCompletedTask"
-                    @removed="handleRemovedTask"
                     />
                 </div>
             </div>
@@ -35,21 +28,12 @@
 import { computed, onMounted, ref} from "vue";
 import { storeToRefs } from "pinia";
 import { useTaskStore } from "../stores/task";
-import {
-    allTasks,
-    createTask,
-    updateTask,
-    completeTask,
-    removeTask,
-} from "../http/task-api";
 import Tasks from "../components/tasks/Tasks.vue";
 import NewTask from "../components/tasks/NewTask.vue";
 
 const store = useTaskStore()
 const { completedTasks, uncompletedTasks } = storeToRefs(store)
 const { fetchAllTasks } = store
-
-const tasks = ref([])
 
 
 onMounted(async () => {
@@ -61,30 +45,4 @@ const showToggleCompletedBtn = computed(() => uncompletedTasks.value.length > 0 
 const completedTasksIsVisible = computed(() => uncompletedTasks.value.length == 0 || completedTasks.value.length > 0);
 
 const showCompletedTasks = ref(false);
-
-const handleAddedTask = async (newTask) => {
-    const { data: createdTask } = await createTask(newTask);
-    tasks.value.unshift(createdTask.data);
-};
-const handleUpdatedTask = async (task) => {
-    const { data: updatedTask } = await updateTask(task.id, {
-        name: task.name,
-    });
-    const currentTask = tasks.value.find((item) => item.id === task.id);
-    currentTask.name = updatedTask.data.name;
-};
-
-const handleCompletedTask = async (task) => {
-    const { data: updatedTask } = await completeTask(task.id, {
-        is_completed: task.is_completed,
-    });
-    const currentTask = tasks.value.find((item) => item.id === task.id);
-    currentTask.is_completed = updatedTask.data.is_completed;
-};
-
-const handleRemovedTask = async (task) => {
-    await removeTask(task.id);
-    const index = tasks.value.findIndex((item) => item.id === task.id);
-    tasks.value.splice(index, 1);
-};
 </script>
