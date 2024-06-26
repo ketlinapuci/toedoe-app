f430cf2be105d682cbd9cf27de2c255e600e5d56
Ketlina Puci <ketlina.puci@evolutivo.io>
2024-06-26 14:49:50 +0200

    Handle Summary filter change

--- src/pages/SummaryPage.vue
+++ src/pages/SummaryPage.vue
@@ -3,21 +3,11 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-8 offset-md-2">
-                    <div
-                        class="d-flex justify-content-between align-items-center mb-4"
-                    >
-                        <h1>
-                            Summary
-                            <small class="text-muted fs-4">{{
-                                selectedFilter
-                            }}</small>
-                        </h1>
-                        <SummaryFilter @update="selectedFilter = $event" />
+                    <div class="d-flex justify-content-between align-items-center mb-4">
+                        <h1> Summary <small class="text-muted fs-4">{{ selectedFilter.text }}</small></h1>
+                        <SummaryFilter @update="setSelectedFilter" />
                     </div>
-                    <div
-                        v-for="(tasks, description) in summaries"
-                        :key="description"
-                    >
+                    <div v-for="(tasks, description) in summaries" :key="description">
                         <Summaries :tasks="tasks" :description="description" />
                     </div>
                 </div>
@@ -28,7 +18,8 @@
 
 <script setup>
 import { useSummaryStore } from "../stores/summary";
-import { onMounted, ref } from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
+//import { useRoute } from "vue-router";
 import { storeToRefs } from "pinia";
 import Summaries from "../components/summaries/Summaries.vue";
 import SummaryFilter from "../components/summaries/filter/SummaryFilter.vue";
@@ -36,10 +27,19 @@ import SummaryFilter from "../components/summaries/filter/SummaryFilter.vue";
 const store = useSummaryStore();
 const { summaries } = storeToRefs(store);
 const { fetchTasksSummary } = store;
-const selectedFilter = ref("");
+const selectedFilter = reactive({
+    period: '',
+    text: '',
+})
 
+const setSelectedFilter = (event) => Object.assign(selectedFilter, event)
 onMounted(async () => {
     await fetchTasksSummary();
 });
+
+// const route = useRoute()
+// watch(() => route.query, async (query) => await fetchTasksSummary( query ))
+
+watch(() => selectedFilter.period, async (period) => await fetchTasksSummary({ period }))
 
 </script>
